1a50de74e1e86bafe55e4c47f9a6ff19a44cd918
soda <wangsuda@icloud.com>
2019-11-18 08:11:09 +0800

    session 13 1:09

--- server/readme.md
+++ server/readme.md
@@ -1,1 +1,3 @@
 `npm i -s express`
+在服务器段需要使用 express-graphql
+`npm i -s graphql express-graphql`
---
+++ server/schema/schema.js
@@ -0,0 +1,67 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+
+const { 
+    GraphQLObjectType, 
+    GraphQLString, 
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLInt
+} = graphql;
+
+// dummy data
+const books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
+
+const authors = [
+    { name: 'Patrick Rothfuss', age: 44, id: '1' },
+    { name: 'Brandon Sanderson', age: 42, id: '2' },
+    { name: 'Terry Pratchett', age: 66, id: '3' }
+];
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString }
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLID }},
+            resolve(parent, args) {
+                // code to get data from db / other source
+                console.log('typeof args.id :', typeof args.id);
+               return _.find(books, { id: args.id });
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: {id: { type: GraphQLID }},
+            resolve(parent, args) {
+                return _.find(authors, { id: args.id });
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
